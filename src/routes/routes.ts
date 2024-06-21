b244389e61172e01b0ae33f87a6f4ff4742a3028
CharllyG23 <charllyfiguera21@gmail.com>
2024-06-21 18:23:37 -0300

    project settings

---
+++ src/routes/routes.ts
@@ -0,0 +1,11 @@
+import HomeViews from '../views/HomeViews/HomeViews.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeViews
+    },
+]
+
+export default routes;
