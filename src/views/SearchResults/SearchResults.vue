d1d5b170553a84e7bb0cad3602e7adba785b961d
andres <andres@socarrao.com.br>
2024-06-22 20:37:02 -0300

    data template

--- src/views/SearchResults/SearchResults.vue
+++ src/views/SearchResults/SearchResults.vue
@@ -1,19 +1,23 @@
 <template>
   <section>
     <h1>Resultados</h1>
-    <ul>
-      <!-- <li v-for="hotel in searchStore.results" :key="hotel.destination.id">
-        <span>{{ hotel.destination.name }}</span>
-        <span>{{ hotel.destination.city }}, {{ hotel.destination.country }}</span>
-        <span>${{ hotel.destination.price }}</span>
-        <span>{{ hotel.destination.rating }} stars</span>
-      </li> -->
-      <div  v-for="(hotel, index) in searchStore.results" :key="index">
-        {{ hotel }}
+    <div v-for="destino in searchStore.results" :key="destino.id_cidade">
+      <h2>{{ destino.cidade }}</h2> <br>
+      <span>-------------------------------------</span>
+      <div v-for="hotel in destino.hoteis" :key="hotel.nome">
+        <h3><strong>Name:</strong> {{ hotel.nome }}</h3>
+        <p><strong>Endereço:</strong> {{ hotel.endereco }}</p>
+        <p><strong>Avaliação:</strong> {{ hotel.avaliacao }}</p>
+        <p><strong>Preço:</strong> R$ {{ hotel.preco }}</p>
+        <p><strong>Número de Quartos:</strong> {{ hotel.numero_quartos }}</p>
+        <p><strong>Máximo de Hóspedes:</strong> {{ hotel.numero_maximo_hospedes }}</p>
+        <p><strong>Datas Disponíveis:</strong></p>
+        <span>-------------------------------------</span>
       </div>
-    </ul>
+    </div>
   </section>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useSearchStore } from '../../store/search';
@@ -26,4 +30,3 @@ export default defineComponent({
   },
 });
 </script>
-
