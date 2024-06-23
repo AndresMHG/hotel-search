6f02171d3a5becdd2b0f22f4b0230c117189a28b
CharllyG23 <charllyfiguera21@gmail.com>
2024-06-23 20:14:34 -0300

    comparison

--- src/components/HsHeader/HsHeader.vue
+++ src/components/HsHeader/HsHeader.vue
@@ -1,15 +1,23 @@
 <template>
-    <header class="header">
-        <div class="header-logo">
-            <h1>Hote|.com</h1>
-        </div>
-        <nav class="header-nav">
-            <a href="#" target="_blank">Comparação</a>
-            <a href="#" target="_blank">Dicas</a>
-        </nav>
-    </header>
+  <header class="header">
+    <div class="header-logo">
+      <hs-icon name="hotel" size="sm" />
+      <h1>Hote|.com</h1>
+    </div>
+    <nav class="header-nav">
+      <a href="#" target="_blank">
+        <strong v-if="!selectedHotels">({{ selectedHotels.length }})</strong> Comparación
+      </a>
+      <a href="#" target="_blank">Dicas</a>
+    </nav>
+  </header>
 </template>
-<script setup>
+<script setup lang="ts">
+import { ref } from 'vue'
+import HsIcon from '../HsIcon/HsIcon.vue';
+
+const selectedHotels = ref([]);
+
 </script>
 <style lang="scss" scoped>
 @import './HsHeaderStyle.scss';
--- src/components/HsIcon/Icons/index.ts
+++ src/components/HsIcon/Icons/index.ts
@@ -1,3 +1,4 @@
 export { default as IconSearch } from './IconSearch.vue';
 export { default as IconChevronDown } from './IconChevronDown.vue';
-export { default as IconStar } from './IconStar.vue'
+export { default as IconStar } from './IconStar.vue';
+export { default as IconHotel } from './IconHotel.vue';
